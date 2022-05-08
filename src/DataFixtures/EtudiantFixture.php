85a3d70f3ad2570fc91f1233dc62a646d5468868
unknown <achraf.habib@insat.ucar.tn>
2022-05-08 16:27:13 +0100

    first commit

--- src/DataFixtures/EtudiantFixture.php
+++ src/DataFixtures/EtudiantFixture.php
@@ -1,37 +1,24 @@
 <?php
 
 namespace App\DataFixtures;
-
 use App\Entity\Etudiant;
-
-use App\Entity\Section;
+use App\Repository\EtudiantRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+
 class EtudiantFixture extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $faker =Factory::create('fr');
-        for ($i=0;$i<20;$i++){
-            $etudiant = new Etudiant() ;
+        $faker = Factory::create('fr');
 
-            $etudiant-> setPrenom($faker->firstName);
-            $etudiant-> setNom($faker->lastName);
-//            {% if $i is even  %}
-//
-//
-//
-//               $etudiant->setSection(APP/ENTITY/Section::class)
-//
-//             {% endif%}
-            $manager->persist($etudiant);
+        for($i=1; $i<11; $i++){
+            $var = new Etudiant();
+            $var->setNom($faker->firstName );
+            $var->setPrenom( $faker->lastName);
 
-
-
-        }
-        // $product = new Product();
-        // $manager->persist($product);
+            $manager->persist($var);}
 
         $manager->flush();
     }
